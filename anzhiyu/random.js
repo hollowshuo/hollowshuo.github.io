f05ee45747db6139149ae8e6bcaaf89b086bdb57
hollowshuo <hollowshuo@126.com>
2025-01-20 21:05:40 +0800

    Site updated: 2025-01-20 21:05:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/18/hello-world/"];function toRandomPost(){
+var posts=["2025/01/20/突然觉得自己就像一只鸵鸟/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
